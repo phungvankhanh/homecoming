4c8f975c1e7d3b81dbcf53e3ba04a19e7e1498e4
toloan <toloan103@gmail.com>
2018-10-10 17:04:18 +0700

    Revert "show destination"

--- homecoming/config/routes.rb
+++ homecoming/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'pagelist/index' 
-  get 'destination/index' , to: 'destination#index'
+  get 'pagelist/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
